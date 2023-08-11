4701df087c67e1bbc5d06df6a78879d4827c0527
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-08-12 00:05:18 +0530

    Add files via upload

---
+++ src/Component/color.js
@@ -0,0 +1,14 @@
+const Color=(props)=> {
+  return (
+    <div className="main">
+      <div className="Cards" style={{ backgroundColor: props.code }}>
+        <div className="Title">
+          <h6>{props.title}</h6>
+          <p>{props.color}</p>
+        </div>
+      </div>
+    </div>
+  );
+  }
+
+export default Color;
